9a210f51b35739f99bcd4fc73f0cdf6929d224b6
Pakize Bozkurt <pakiozihak@gmail.com>
2023-07-30 21:44:55 +0100

    added my info

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,21 +8,19 @@ function NavBar() {
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
           <div className="container-fluid">
-            
-              <button type="button" className="btn btn-secondary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-stars"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z"></path>
-                </svg>
-              </button>
-            
-            
+            <button type="button" className="btn btn-secondary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-stars"
+                viewBox="0 0 16 16"
+              >
+                <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z"></path>
+              </svg>
+            </button>
+
             <button
               className="navbar-toggler"
               type="button"
@@ -55,6 +53,26 @@ function NavBar() {
             </div>
           </div>
         </nav>
+      </div>
+      <div className="container-fluid">
+        <div className=" px-4 py-5 my-5 text-center">
+          <img
+            className="img-fluid"
+            src="portfolio/src/Images/PHOTO-2023-04-27-20-13-23.jpg"
+            alt="Pakize's foto"
+          />
+          <h1 className="display-5 fw-bold text-body-emphasis">I'm Pakize</h1>
+          <div class="text-center lh-lg">
+            <div className="card-body">
+              <h5 className="card-title fs-1 fw-semibold lh-lg">
+                Full Stack / Software Developer
+              </h5>
+              <p className="card-text fs-2 fw-medium lh-lg">
+                Living for learning.....
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
