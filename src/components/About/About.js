23a34bbb81825da02f304f059dc165c628ad1317
Pakize Bozkurt <pakiozihak@gmail.com>
2023-07-31 13:05:05 +0100

    added my skills in about file

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,34 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
 
 function About() {
   return (
     <div>
-      
+      <section className="py-5 text-center container">
+        <div className="row py-lg-5">
+          <div className="col-lg-6 col-md-8 mx-auto">
+            <h1 className="fw-light">My Skills</h1>
+            <p className="lead text-body-secondary">
+              As a Full Stack Developer, I have completed a comprehensive course
+              and acquired expertise in HTML, CSS, JavaScript, React, Node.js,
+              SQL, and Agile Methodology. My skills in SDLC enable me to build
+              responsive user interfaces, efficient back-end solutions, and
+              manage databases effectively, delivering high-quality products
+              that meet user needs.
+            </p>
+            <p>
+              <a
+                href="https://github.com/PakizeBozkurt"
+                className="btn btn-secondary my-2"
+              >
+                See My Learning Jurney
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default About;
+export default About
