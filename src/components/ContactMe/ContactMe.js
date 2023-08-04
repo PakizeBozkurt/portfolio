0a0a9c54b7377d6722f9f6c8edd5981b484ae7f2
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-04 23:48:09 +0100

    styled contact info

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -2,17 +2,15 @@ import React from 'react';
 
 function ContactMe() {
   return (
-    <div>
-      <section className="contact-wrap" id="Contact">
-        <div className="content">
-          <h1>
-            Contact 
-          </h1>
+    <div className="d-flex justify-content-center p-5">
+      <section className="contact-wrap bg-light py-5" id="Contact">
+        <div className="content text-center my-5">
+          <h1 className="display-5 fw-bolder">Contact</h1>
           <p>
             Hey, send me an email if you want to connect! Additionally, you can
             find me on{" "}
             <a
-              className="linked-in"
+              className="linked-in fw-bold"
               href="https://www.linkedin.com/in/pakize-bozkurt/"
               target="_blank"
               rel="noopener noreferrer"
@@ -21,8 +19,7 @@ function ContactMe() {
             </a>
             , shoot a message if you have to!
           </p>
-          <a className="email" href="mailto:pakiozihak@gmail.com">
-          
+          <a className="email fw-bold" href="mailto:pakiozihak@gmail.com">
             pakiozihak@gmail.com
           </a>
         </div>
