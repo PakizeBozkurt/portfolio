49844f72f0e558b90cac5f9252a3d5f1b4b7c127
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-06 22:43:36 +0100

    added skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import "./Skills.css";
 
 function Skills() {
   return (
@@ -11,12 +12,55 @@ function Skills() {
                 <h2 className="display-5 fw-bolder">
                   <span className="text-gradient d-inline">About Me</span>
                 </h2>
-                <p className="lead fw-light mb-4">
-                  My name is Pakize,
-                </p>
+                <p className="lead fw-light mb-4">My name is Pakize,</p>
                 <p className="text-muted">
-                  I love creating websites with user-friendly. Living for learning....
+                  I love creating websites with user-friendly. Living for
+                  learning....
                 </p>
+                <div className="skills">
+                  <div className="skills-container">
+                    <div className="skills-section">
+                      <label className="skills-section-title">Skills</label>
+                      <div className="skills-list">
+                        <div className="skill-card">
+                          <div className="skill-icon">
+                            <label className="skill-name">ReactJS</label>
+                          </div>
+                        </div>
+                        <div className="skill-card">
+                          <div className="skill-icon">
+                            <label className="skill-name">HTML5</label>
+                          </div>
+                        </div>
+                        <div className="skill-card">
+                          <div className="skill-icon">
+                            <label className="skill-name">CSS3</label>
+                          </div>
+                        </div>
+                        <div className="skill-card">
+                          <div className="skill-icon">
+                            <label className="skill-name">JavaScript</label>
+                          </div>
+                        </div>
+                        <div className="skill-card">
+                          <div className="skill-icon">
+                            <label className="skill-name">Node.JS</label>
+                          </div>
+                        </div>
+                        <div className="skill-card">
+                          <div className="skill-icon">
+                            <label className="skill-name">SQL</label>
+                          </div>
+                        </div>
+                        <div className="skill-card">
+                          <div className="skill-icon">
+                            <label className="skill-name">MongoDB</label>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
                 <div className="d-flex justify-content-center fs-2 gap-4">
                   <a className="text-gradient" href="#!">
                     <i className="bi bi-twitter"></i>
