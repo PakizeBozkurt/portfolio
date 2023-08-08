375bcbc42e70e82a14799b74df36e2311b457162
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-08 22:12:32 +0100

    added icons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import NavBar from "./components/NavBar/NavBar";
 import Footer from './components/Footer/Footer';
 import "./App.css"; 
 import Pages from "./pages/Pages";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 
 
 function App() {
