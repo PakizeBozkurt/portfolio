eff1a956acde8eb910a99b7374f5c5e6e541d63c
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-09 23:18:03 +0100

    styled

--- src/components/Skills/SkillCard.js
+++ src/components/Skills/SkillCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function SkillCard({ name }) {
   return (
-    <div className=" d-flex flex-wrap justify-content-between algin-items-center bg-danger p-5 m-2">
+    <div className=" d-flex flex-wrap justify-content-between algin-items-center bg-secondary p-5 m-2">
       <label className="skill-name">{name}</label>
     </div>
   );
