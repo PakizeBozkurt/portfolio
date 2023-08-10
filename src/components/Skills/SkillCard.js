2e617121d444c562c44d4e9df29ee7b8bbea9934
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-10 23:01:40 +0100

    added css

--- src/components/Skills/SkillCard.js
+++ src/components/Skills/SkillCard.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Skill.css";
 
 function SkillCard({ name }) {
   return (
-    <div className=" d-flex flex-wrap justify-content-between algin-items-center bg-secondary p-5 m-2">
+    <div className=" d-flex flex-wrap justify-content-between algin-items-center bgCircle p-5 m-2">
       <label className="skill-name">{name}</label>
     </div>
   );
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -22,21 +22,21 @@ function Skills() {
       <div className="skills">
         <div className="skills-container">
           <div className="skills-section px-5 fs-4 ">
-            <label className="fs-1 my-5 py-5 ">Skills</label>
-            <div className=" d-flex flex-wrap justify-content-between algin-items-center">
-              <FaReact className="fs-1  bg-warning" />
+            <label className="fs-1 my-5 py-5 bgCircle ">Skills</label>
+            <div className=" d-flex flex-wrap justify-content-between algin-items-center ">
+              <FaReact className="fs-1  " />
               <SkillCard name="ReactJS" />
-              <FaHtml5 className="fs-1 ms-3 bg-warning" />
+              <FaHtml5 className="fs-1 ms-3 " />
               <SkillCard name="HTML5" />
-              <FaCss3Alt className="fs-1 ms-3 bg-warning" />
+              <FaCss3Alt className="fs-1 ms-3" />
               <SkillCard name="CSS3" />
-              <BiLogoJavascript className="fs-1 ms-3 bg-warning" />
+              <BiLogoJavascript className="fs-1 ms-3 " />
               <SkillCard name="JavaScript" />
-              <FaNode className="fs-1 ms-3 bg-warning" />
+              <FaNode className="fs-1 ms-3 " />
               <SkillCard name="NodeJS" />
-              <BiLogoPostgresql className="fs-1 ms-3 bg-warning" />
+              <BiLogoPostgresql className="fs-1 " />
               <SkillCard name="SQL" />
-              <BiLogoMongodb className="fs-1 ms-3 bg-warning" />
+              <BiLogoMongodb className="fs-1 ms-3 " />
               <SkillCard name="MongoDB" />
             </div>
           </div>
