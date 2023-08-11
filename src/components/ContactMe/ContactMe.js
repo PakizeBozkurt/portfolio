c511a6a2a0398a5bd592f7db0300623c3fd71356
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-11 22:58:41 +0100

    changed title

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -6,7 +6,7 @@ function ContactMe() {
     <div className=" p-5 mx-auto">
       <section className="contact-wrap bg-light py-5" id="Contact">
         <div className="content text-center my-5">
-          <h1 className="display-5 fw-bolder p-5 my-5">Contact</h1>
+          <h1 className="display-5 fw-bolder p-5 my-5">Get in Touch!</h1>
           <p className="fs-3 p-5 my-5">
             Hey, send me an email if you want to connect! Additionally, you can
             find me on
