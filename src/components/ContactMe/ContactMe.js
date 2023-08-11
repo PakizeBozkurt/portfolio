6efacea13446994ff8d2f08995792deddd99fc72
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-11 23:16:46 +0100

    added font size

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -7,11 +7,10 @@ function ContactMe() {
       <section className="contact-wrap bg-light py-5" id="Contact">
         <div className="content text-center my-5">
           <h1 className="display-5 fw-bolder p-5 my-5">Get in Touch!</h1>
-          <p className="fs-3 p-5 my-5">
-            Hey, send me an email if you want to connect! 
-            <SocialMediaLinks />
+          <p className="fs-2 p-5 my-5">
+            Hey, send me an email or get in touch, if you want to connect!
           </p>
-        
+          <SocialMediaLinks />
         </div>
       </section>
     </div>
--- src/components/ContactMe/SocialMediaLinks.js
+++ src/components/ContactMe/SocialMediaLinks.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function SocialMediaLinks() {
   return (
-    <div className="d-flex justify-content-center fs-2 gap-4">
+    <div className="d-flex justify-content-center fs-2 gap-4 ">
       <a
         className="text-gradient"
         href="https://www.linkedin.com/in/pakize-bozkurt/"
         target="_blank"
         rel="noreferrer"
       >
-        <i className="bi bi-linkedin"></i>
+        <i className="bi bi-linkedin "></i>
       </a>
       <a
         className="text-gradient"
