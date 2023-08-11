11546705a3b46d158e70d4f93f09f9555f1fb7bb
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-11 23:33:28 +0100

    styled buttons

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ import React from "react";
 function About() {
   return (
     <div>
-      <section className="py-5 text-center container">
+      <section className="py-5 my-5 m-5 text-center container ">
         <div className="row py-lg-5">
           <div className="col-lg-6 col-md-8 mx-auto text-center">
             <h1 className="fw-bold m-0">My Skills</h1>
@@ -23,7 +23,9 @@ function About() {
             <p>
               <a
                 href="https://github.com/PakizeBozkurt"
-                className="btn btn-secondary fw-bold my-2"
+                className="btn btn-secondary fw-bold my-2 border border-0"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 Go My Learning Jurney
               </a>
@@ -31,17 +33,17 @@ function About() {
           </div>
         </div>
       </section>
-      <div className="row">
-        <div className="col-sm-6 mb-3 mb-sm-5">
-          <div className="card">
-            <div className="card-body">
+      <div className="row m-5 px-5 ">
+        <div className="col-sm-6 mb-3 mb-5  ">
+          <div className="card  ">
+            <div className="card-body ">
               <h5 className="card-title fs-1 fw-bold">HTML & CSS</h5>
               <p className="card-text">
                 Created an MVP Project with HTML & CSS
               </p>
               <a
                 href="https://volunteer-group.netlify.app/"
-                className="btn btn-secondary fw-bold"
+                className="btn btn-secondary fw-bold border border-0"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -59,7 +61,7 @@ function About() {
               </p>
               <a
                 href="https://hck2022.netlify.app/"
-                className="btn btn-secondary fw-bold"
+                className="btn btn-secondary fw-bold border border-0"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -68,16 +70,16 @@ function About() {
             </div>
           </div>
         </div>
-        <div className="col-sm-6">
+        <div className="col-sm-6 ">
           <div className="card">
             <div className="card-body">
-              <h5 className="card-title fs-1 fw-bold">React</h5>
+              <h5 className="card-title fs-1 fw-bold ">React</h5>
               <p className="card-text">
                 Created a Hotel Project for Code Your Future with React.
               </p>
               <a
                 href="https://reacthotelapp.netlify.app/"
-                className="btn btn-secondary fw-bold"
+                className="btn btn-secondary fw-bold border border-0"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -91,11 +93,12 @@ function About() {
             <div className="card-body">
               <h5 className="card-title fs-1 fw-bold">Full Stack</h5>
               <p className="card-text">
-              Comleted a Full stack(React.js, Node.js and PostgreSQL) project with 4 team members in 4 weeks successfully!
+                Comleted a Full stack(React.js, Node.js and PostgreSQL) project
+                successfully!
               </p>
               <a
                 href="https://github.com/PakizeBozkurt/hugsforbugs"
-                className="btn btn-secondary fw-bold"
+                className="btn btn-secondary fw-bold border border-0"
                 target="_blank"
                 rel="noopener noreferrer"
               >
