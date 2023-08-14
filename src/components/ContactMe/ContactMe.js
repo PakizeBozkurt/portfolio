b28d8ddf015c1fafed86a0af76171e48e772fb0f
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-14 21:27:27 +0100

    added text colour get in touch

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -4,7 +4,7 @@ import SocialMediaLinks from "./SocialMediaLinks";
 function ContactMe() {
   return (
     <div className="contact-wrap bg-light mx-auto py-5 rounded-5 w-50 h-25 bg-dark ">
-      <div className="content text-center my-5 ">
+      <div className="content text-center my-5 text-light">
         <h1 className="display-5 fw-bolder p-5 my-5 ">Get in Touch!</h1>
         <p className="fs-2 p-5 my-5">
           Hey, send me an email or get in touch, if you want to connect!
