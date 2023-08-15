8253bb5f975ba4311d1fa991cbfd26fc5d76c3cf
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-15 23:14:59 +0100

    added new img

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,22 +1,17 @@
 import React from "react";
-// import "./Heading.css";
 
 function Heading() {
   return (
-    <div className="bg-image">
-      <div className="container-fluid">
-        <div className="d-flex align-items-center px-3 py-3 my-3">
-          <img
-            src="../assets/Images/PHOTO-2023-04-27-20-13-23.jpg"
-            className="img-fluid p-5  mx-auto  rounded-circle border border-dark"
-            alt="Pakize Bozkurt"
-            width="300"
-            height="200"
-          />
-          <h1 className="fw-bold text-body-emphasis text-center mx-5 px-5">
-            Full Stack Web Developer <br /> & <br /> Software Developer
-          </h1>
-        </div>
+    <div className="container-fluid">
+      <div className="d-flex align-items-center px-3 py-3 my-3">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-wv1RmRX9FkY-CDJXOq5sbZQ1F06i9cXTVw&usqp=CAU"
+          className="img-fluid p-5  mx-auto  rounded-circle border border-end border-5"
+          alt="Pakize Bozkurt"
+        />
+        <h1 className="fw-bold text-body-emphasis text-center mx-5 px-5">
+          Full Stack Web Developer <br /> & <br /> Software Developer
+        </h1>
       </div>
     </div>
   );
