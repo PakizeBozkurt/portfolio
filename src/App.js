b98a8e724fdb90f5dc7aa276464a3e70869b8b95
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-17 23:29:30 +0100

    formating

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
-import Footer from './components/Footer/Footer';
-import "./App.css"; 
+import Footer from "./components/Footer/Footer";
+import "./App.css";
 import Pages from "./pages/Pages";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-
-
 
 function App() {
   return (
