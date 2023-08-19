33335fc61d7ba2ef9b749289c6386b6bb070f5d2
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-19 22:23:42 +0100

    added contact file

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from "react";
+import SocialMediaLinks from "./SocialMediaLinks";
 
 function Contact() {
   return (
-    <div>
-      
+    <div className="contact-wrap bg-light mx-auto  rounded-5 w-50 h-25 bg-dark ">
+      <div className="content text-center my-5 text-light">
+        <h1 className="display-7 fw-bolder p-5 my-5 ">Hire Me!!!</h1>
+        <p className="fs-2 p-5 my-5">
+          I am passionate about user-firendly application. <br /> I am also open
+          new challenges.
+          <br />
+          <br /> Just hire me!
+        </p>
+        <mark className="bg-dark text-light fs-3"> Follow! </mark>
+        <br />
+        <br />
+        <SocialMediaLinks />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
-
-
-
-
-
+export default Contact;
