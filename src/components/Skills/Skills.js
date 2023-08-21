b9fdf107e5ff81da0d64ceca7ac1a47f5ae2ccfb
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-21 22:34:49 +0100

    refactoring the skills section

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,43 +1,44 @@
 import React from "react";
 import SkillCard from "./SkillCard";
-import { FaReact } from "react-icons/fa";
-import { FaHtml5 } from "react-icons/fa";
-import { FaCss3Alt } from "react-icons/fa";
-import { BiLogoJavascript } from "react-icons/bi";
-import { FaNode } from "react-icons/fa";
-import { BiLogoMongodb } from "react-icons/bi";
-import { BiLogoPostgresql } from "react-icons/bi";
+import { FaReact, FaHtml5, FaCss3Alt, FaNode } from "react-icons/fa";
+import {
+  BiLogoJavascript,
+  BiLogoMongodb,
+  BiLogoPostgresql,
+} from "react-icons/bi";
 import "./Skill.css";
+
+const skillsData = [
+  { icon: <FaReact />, name: "ReactJS" },
+  { icon: <FaHtml5 />, name: "HTML5" },
+  { icon: <FaCss3Alt />, name: "CSS3" },
+  { icon: <BiLogoJavascript />, name: "JavaScript" },
+  { icon: <FaNode />, name: "NodeJS" },
+  { icon: <BiLogoPostgresql />, name: "SQL" },
+  { icon: <BiLogoMongodb />, name: "MongoDB" },
+];
 
 function Skills() {
   return (
     <div className="text-center my-5 p-5">
       <h2 className="display-5 fw-bolder p-5 my-5">
-        <span className="text-gradient d-inline ">About Me</span>
+        <span className="text-gradient d-inline">About Me</span>
       </h2>
-      <p className="lead fw-bold mb-4 fs-1 p-5 ">My name is Pakize,</p>
-      <p className=" fs-3">
+      <p className="lead fw-bold mb-4 fs-1 p-5">My name is Pakize,</p>
+      <p className="fs-3">
         I love creating websites with user-friendly. Living for learning....
       </p>
       <div className="skills">
         <div className="skills-container">
-          <div className="skills-section px-5 fs-4 ">
-            <label className="fs-1 my-5 py-5 bgCircle  ">Skills</label>
-            <div className=" d-flex flex-wrap justify-content-between algin-items-center ">
-              <FaReact className="fs-1 ms-3 icon" />
-              <SkillCard name="ReactJS" />
-              <FaHtml5 className="fs-1 ms-3 icon" />
-              <SkillCard name="HTML5" />
-              <FaCss3Alt className="fs-1 ms-3 icon" />
-              <SkillCard name="CSS3" />
-              <BiLogoJavascript className="fs-1 ms-3 icon" />
-              <SkillCard name="JavaScript" />
-              <FaNode className="fs-1 ms-3 icon" />
-              <SkillCard name="NodeJS" />
-              <BiLogoPostgresql className="fs-1 icon" />
-              <SkillCard name="SQL" />
-              <BiLogoMongodb className="fs-1 ms-3 icon" />
-              <SkillCard name="MongoDB" />
+          <div className="skills-section px-5 fs-4 icon">
+            <label className="fs-1 my-5 py-5 bgCircle ">Skills</label>
+            <div className="d-flex flex-wrap justify-content-between align-items-center">
+              {skillsData.map(({ icon, name }) => (
+                <React.Fragment key={name}>
+                  {icon}
+                  <SkillCard name={name} />
+                </React.Fragment>
+              ))}
             </div>
           </div>
         </div>
