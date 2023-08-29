1bfa7eaf113ece6748b2da2d521383cad5530671
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-29 14:49:25 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Pages from "./pages/Pages";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
+
 function App() {
   return (
     <div className="app-container">
